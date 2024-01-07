6dd86ccdde1cd4fb42d9cccd4e7503114b036c70
Sensei Keqing <k.izuka@yandex.com>
2024-01-07 11:53:23 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,9 +1,28 @@
-import uvloop
+import os, uvloop
 
+from logging import (
+    getLogger,
+    FileHandler,
+    StreamHandler,
+    INFO,
+    basicConfig
+)
 from pyrogram import Client
 from bot.config import Config
 
 uvloop.install() # Pyrogram Speed Up
+
+if os.path.exists('log.txt'):
+    with open('log.txt', 'w+') as f:
+        f.truncate(0)
+
+basicConfig(
+    format="%(asctime)s - [%(filename)s:%(lineno)d] - %(levelname)s - %(message)s",
+    handlers=[FileHandler("log.txt"), StreamHandler()],
+    level=INFO,
+)
+
+LOGGER = getLogger(__name__)
 
 admins = list(Config.admins)
 prefix = ["/","!","#","-","?"]
