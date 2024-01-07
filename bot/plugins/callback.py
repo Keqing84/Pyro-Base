24ebfada7e8bdad71992b063c28cee640fb33ac0
Sensei Keqing <k.izuka@yandex.com>
2024-01-07 14:07:01 +0530

    Create callback.py

---
+++ bot/plugins/callback.py
@@ -0,0 +1,24 @@
+from bot import app
+from bot.utils.pre_text import *
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+
+
+@app.on_callback_query()
+async def call_back(_, cb_query: CallbackQuery):
+   data = cb_query.data
+   msg = cb_query.message
+   if data == "closee":
+     try:
+        await msg.delete()
+     except:
+        pass
+   if data == "mainn":
+     try:
+        await msg.edit_caption(start_text.format(f"[{msg.chat.first_name}](tg://user?id={msg.chat.id})"), reply_markup=start_mark)
+     except:
+        pass
+   if data == "helpp":
+     try:
+        await msg.edit_caption(help_text, reply_markup=help_mark)
+     except:
+        pass
