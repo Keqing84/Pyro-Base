231439e83d837aa277eea4d55aecca99044867fd
Sensei Keqing <k.izuka@yandex.com>
2024-11-11 14:36:24 +0530

    Update mongo.py

--- bot/database/mongo.py
+++ bot/database/mongo.py
@@ -19,7 +19,7 @@ _id || Auto Generate By Mongo, So Set The Tg Id In It
 
 class MongoPy:
     def __init__(self, uri, bot="KAI84", coll="bot"):
-        self.client = pymongo.mongo_client.MongoClient(uri)
+        self.client = pymongo.MongoClient(uri)
         self.mdb = self.client[bot]
         self.coll = self.mdb[coll]
         self.connected = self.ping()
