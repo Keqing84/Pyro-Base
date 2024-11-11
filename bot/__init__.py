ca8446039d8ab35b79d97e45be306669aef53fcf
Sensei Keqing <k.izuka@yandex.com>
2024-11-11 14:42:33 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,29 +1,9 @@
 import os, uvloop
-
-from logging import (
-    getLogger,
-    FileHandler,
-    StreamHandler,
-    INFO,
-    basicConfig
-)
 from pyrogram import Client
 from bot.config import Config
 from bot.database.mongo import MongoPy
 
 uvloop.install() # Pyrogram Speed Up
-
-if os.path.exists('log.txt'):
-    with open('log.txt', 'w+') as f:
-        f.truncate(0)
-
-basicConfig(
-    format="%(asctime)s - [%(filename)s:%(lineno)d] - %(levelname)s - %(message)s",
-    handlers=[FileHandler("log.txt"), StreamHandler()],
-    level=INFO,
-)
-
-LOG = getLogger(__name__)
 
 admins = list(Config.admins)
 prefix = ["/","!","#","-","?"]
@@ -31,10 +11,11 @@ db_uri = Config.db_url
 
 Db = MongoPy(db_uri)
 
-app = Client("Pyro-Base",
-        api_id=Config.api_id,
-        api_hash=Config.api_hash,
-        bot_token=Config.bot_token,
-        workdir=os.path.dirname(__file__),
-        plugins=dict(root="plugins")
-     )
+app = Client(
+    "Pyro-Base",
+    api_id=Config.api_id,
+    api_hash=Config.api_hash,
+    bot_token=Config.bot_token,
+    workdir="bot",
+    plugins={'root': 'bot.plugins'}
+    )
