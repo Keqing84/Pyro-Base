0bacd16f86b22ae566419ef897eaf9e2a3595ef3
Sensei Keqing <k.izuka@yandex.com>
2024-11-19 23:38:32 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -5,20 +5,25 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQ
 
 @app.on_callback_query()
 async def call_back(_, cb_query: CallbackQuery):
-   data = cb_query.data
-   msg = cb_query.message
-   if data == "closee":
-     try:
-        await msg.delete()
-     except:
-        pass
-   if data == "mainn":
-     try:
-        await msg.edit_caption(start_text.format(f"[{msg.chat.first_name}](tg://user?id={msg.chat.id})"), reply_markup=start_mark)
-     except:
-        pass
-   if data == "helpp":
-     try:
-        await msg.edit_caption(help_text, reply_markup=help_mark)
-     except:
-        pass
+     data = cb_query.data
+     msg = cb_query.message
+     if data == "closee":
+         try:
+             await msg.delete()
+         except:
+             pass
+     elif data == "mainn":
+         try:
+             await msg.edit_caption(start_text.format(f"[{msg.chat.first_name}](tg://user?id={msg.chat.id})"), reply_markup=start_mark)
+         except:
+             pass
+     elif data == "helpp":
+         try:
+             await msg.edit_caption(help_text, reply_markup=help_mark)
+         except:
+             pass
+     else:
+         try:
+             await msg.edit_caption(data+"\nData Passed On.")
+         except:
+             pass
